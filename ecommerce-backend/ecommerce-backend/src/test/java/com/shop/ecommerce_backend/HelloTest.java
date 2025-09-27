3cffd1806b5bc13ff2fd92d757cb4e2b9a90abba
marthak1 <marthaogunbiyi196@gmail.com>
2025-09-27 17:58:32 +0100

    update with test properties

--- ecommerce-backend/ecommerce-backend/src/test/java/com/shop/ecommerce_backend/HelloTest.java
+++ ecommerce-backend/ecommerce-backend/src/test/java/com/shop/ecommerce_backend/HelloTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 // This test starts the Spring Boot app on a random port, sends a GET request to /api/hello, and checks that the response is "Hello, World!".
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class HelloTest {
 
     @Autowired 
