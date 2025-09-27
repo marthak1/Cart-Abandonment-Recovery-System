b88dda8271bec6b59a82ca98475b64f75fecec10
marthak1 <marthaogunbiyi196@gmail.com>
2025-09-27 16:37:59 +0100

    updated work flow

--- ecommerce-backend/ecommerce-backend/wait-for-db.sh
+++ ecommerce-backend/ecommerce-backend/wait-for-db.sh
@@ -2,11 +2,14 @@
 
 echo "Waiting for MySQL to be ready..."
 
-# Wait until MySQL responds on port 3306
-until nc -z db 3306; do
+until nc -z localhost 3306; do
   echo "MySQL not ready yet. Retrying in 2s..."
   sleep 2
 done
 
-echo "✅ MySQL is up. Running tests..."
-./mvnw test -Dspring.profiles.active=test
+echo "MySQL is up. Running tests..."
+./mvnw test \
+  -Dspring.datasource.url=jdbc:mysql://localhost:3306/cartdb_docker \
+  -Dspring.datasource.username=root \
+  -Dspring.datasource.password=${SPRING_DATASOURCE_PASSWORD}
+
